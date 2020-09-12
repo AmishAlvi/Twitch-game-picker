18ac9343035c39b9d7316563739f24a0e6095107
AmishAlvi <amish.alvi@gmail.com>
2020-09-12 09:00:25 +0300

    initial commit

---
+++ src/GamePicker.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react'
+import { Table } from 'react-bootstrap'
+import {Spinner} from 'react-bootstrap'
+const clientId = process.env.REACT_APP_CLIENT_ID;
+
+class GamePicker extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoaded: false,    // to track fetching and laoding games
+            isSubmitted: false, // to track average view submittion
+            isCalculated: false, // to track final calculation completion
+            average_viewers: '', // to store user average view count
+            queries: 0 // track number of games currently being queried
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    //gaining access to props passed from Gameloader.js
+    access_info = this.props.access_info // gains access to OAuth token and client secret
+    games = this.props.validgames // gains access to validated list of live games 
+    ten_game = [] // array to keep track of games use will be top ten in
+
+    //sets average viewers to sue submitted value
+    handleChange(event) {
+        this.setState({
+            average_viewers: event.target.value,
+        });
+    }
+
+    /* after the user submits their average viewer count
+    ** this code block fetches the top ten streams for all the validated games
+    ** Then compares the viewer range to the average value submitted
+    ** for games in which the average value is greater than the tenth stream's value
+    ** the viewer will appear in teh top and this the game is added to the ten_game array to be shown to user
+    */
+    async handleSubmit(event) {
+        this.setState({
+            isLoaded: true
+        });
+        event.preventDefault(); // prevent page from reloading on form submission
+        var games_url = new URL('https://api.twitch.tv/kraken/streams/')
+        
+
+        for(var m = 0; m < this.games.length; m++) // iterating over eeach valid game using "m" to fetch data fro individual games
+        {
+            this.setState({
+                queries: (this.games.length - m) // tracking the number of games queried to be shopwn to user
+            })
+
+            // this next code block defines params and fetches the top ten streams for the current game "m"
+            var params = {game: this.games[m].game.name, limit: 10}
+            games_url.search = new URLSearchParams(params).toString();
+            await fetch(games_url, {
+    
+                method: 'GET',
+                headers: {
+                    Accept: 'application/vnd.twitchtv.v5+json',
+                    //Authorization: 'Bearer {this.access_info.access_token}',
+                    'Client-ID': clientId
+                }
+            })
+            .then(res => res.json())
+            //after fetching the top ten streams this code block compares the view count of the 10th stream to the users average view count
+            .then(json => {
+                if(this.state.average_viewers > Number(json.streams[json.streams.length - 1].viewers)) 
+                {
+                    this.ten_game.push(this.games[m])
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
+            
+        }
+        
+        // confirming that the final list has been calculated and can now be renderd
+        this.setState({
+            isCalculated: true
+        });
+    
+
+    }
+    
+    
+    render() {
+        const{isLoaded, isCalculated, queries} = this.state; // getting access to states
+
+        // checks if user input has been loaded, and renders a form for input if not
+        if(!isLoaded)
+        {
+            return(
+                <form onSubmit={this.handleSubmit}>
+                    <label><h3>Please enter your average view count:
+                    <input type="number" value={this.state.average_viewers} onChange={this.handleChange} />
+                    </h3></label>
+                    <input type="submit" value="Submit" />
+                </form>
+            )
+        }
+        
+        //checks if final list of games has been calculated and renders a progress bar of games being quesried currently if not
+        if(!isCalculated)
+        {
+            return(
+                <div className="center">
+                    <h3>Please wait while we query over {queries} games</h3>
+                    <Spinner animation="border"  variant="light"/>
+                </div>
+
+            )
+        }
+            // renders table with final list of games
+            return (
+                <div>
+                    <h4>With an average viewership of {this.state.average_viewers} you will appear in the top ten in these categories</h4>
+                    <Table bordered variant="dark" size="sm">
+                        <tbody>
+                        <tr>
+                            <td><h3>Box</h3></td>
+                            <td><h3>Game</h3></td>
+                            <td><h3>Total Viewers</h3></td>
+                            <td><h3>Total Channels</h3></td>
+                        </tr>
+                    {this.ten_game.map(value => { //mapping values from the final games list and rendering them one by one in the table
+                        return(
+                                <tr>   
+                                    <td><a href={'https://twitch.tv/directory/game/' + value.game.name} target='_blank'><img src={value.game.box.small} /></a></td>
+                                    <td><a href={'https://twitch.tv/directory/game/' + value.game.name} target='_blank'>{value.game.name}</a></td>
+                                    <td>{value.viewers}</td>
+                                    <td>{value.channels}</td>
+                                </tr>
+                            
+                        )
+                    })}
+                    </tbody>
+                    </Table>
+                </div>
+            )
+        
+    }
+}
+
+export default GamePicker
---
+++ src/Gameloader.js
@@ -0,0 +1,159 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import GamePicker from './GamePicker'
+import {Spinner} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+
+// defining twitch client secret and id for fetch requests
+const clientId = process.env.REACT_APP_CLIENT_ID;
+const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
+console.log(process.env.REACT_APP_CLIENT_SECRET)
+
+
+//initial laoding component for fetching and validating games on initial load
+class Gameloader extends Component {
+
+      /**
+     * constructor
+     *
+     * @object  @props  parent props
+     * @object  @state  component state
+     */
+    constructor(props) {
+
+      super(props);
+
+      this.state = {
+          access_info: [],
+          isLoaded: false,
+          games: [],
+          loadedGames: 1100
+      }
+
+     
+
+    }
+
+
+  games_array = []  // array of validated games (i.e games over 20 streamers in em)
+  
+  async componentDidMount() {
+
+    // fetching OAuth token from twitch and assigning to reusable state
+    await fetch('https://id.twitch.tv/oauth2/token?client_id=' + clientId  + '&client_secret=' + clientSecret +'&grant_type=client_credentials', {
+        method: 'post',
+        //mode: 'no-cors',
+        })
+        .then(res => res.json())
+        .then(json => {
+            this.setState({
+                access_info: json,
+                
+            })
+        }).catch((err) => {
+            console.log(err);
+        });
+    
+    
+    /* this next block fetches the top 1000 games on twitch 100 at a time 
+    ** once games are fetched if checks if there are more than 20 streamers in the category
+    ** this is to verify that the category is not dead
+    ** Once verified the games that meet the requirements are added into 
+    ** games_array
+    */
+    var url = new URL('https://api.twitch.tv/kraken/games/top')
+    for(var i = 0; i < 1100; i += 100)
+    {
+        var params = {offset: i, limit: 100}
+        url.search = new URLSearchParams(params).toString();
+        await fetch(url, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/vnd.twitchtv.v5+json',
+                Authorization: 'Bearer {this.access_info.access_token}',
+                'Client-ID': clientId
+            }
+        })
+        .then(res => res.json())
+        .then(json => {
+           
+                for(var k = 0; k < json.top.length; k++)
+                {
+                    if(Number(json.top[k].channels) > 20)
+                    {
+                        this.setState({
+                            loadedGames: 1100 - i
+                        })
+
+                        this.games_array.push(json.top[k])
+                        //console.log(this.games_array[k].game.name)
+                        
+                    }
+                }
+
+                
+            
+            
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
+
+    //set loaded state to tell app that games have succesfully laoded and the app can now render
+    this.setState({
+        isLoaded: true, 
+        games: this.games_array
+    })
+
+    //console.log(this.games_array)
+    
+  }
+
+    // twitch and donate buttons on home page
+    btnClickDonate() {
+        window.open("https://streamelements.com/twinkietalks/tip");
+    }   
+    btnClickTwitch() {
+        window.open("https://twitch.tv/twinkietalks");
+    } 
+
+  render() {
+
+    // getting access to states
+    const { isLoaded,  games, access_info, loadedGames} = this.state;
+    
+    // defining props to be passed on to Gamepicker component
+    let props = {
+        validgames: games,
+        access_info: access_info
+    }
+
+    // checks to see if games list has been loaded, if it hasnt loaded, show use an initial loading screen
+    if (!isLoaded)
+        return <div className="center"><h3>Please wait while we load the top {loadedGames} games from twitch</h3><Spinner animation="border"  variant="light"/></div>;
+
+    // Creating home page and launching game pciker component
+    return(
+        <div className="component">
+            <label><h1>Twitch Game Picker</h1>
+            <p>By Twinkietalks</p></label>
+            <p>How It Works:</p>
+            <ul>
+                <li>
+                We first get all categories from twitch and select the ones that have more than 20 streamers live right now (i.e not a dead category)
+                </li>    
+                <li>
+                Then using your average we calculate if you'll be in the top ten streamers in these categories
+                </li>
+            </ul>
+
+            <p> You can use this info to get get a good position and grow! <span role="img">😄</span> </p>
+            <p>If you find this application helpful consider <Button variant="info"  onClick={this.btnClickDonate.bind(this)}>Donating</Button> and Follow me on <Button onClick={this.btnClickTwitch.bind(this)}> Twitch </Button> </p>
+            <GamePicker {...props} />    
+        </div>
+    )
+  }
+}
+export default Gameloader;
+
